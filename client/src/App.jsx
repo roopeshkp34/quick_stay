ba78517f7fc79d6c4b453462ea97739dae951dc8
Roopesh Kp <68722725+roopeshkp34@users.noreply.github.com>
2025-05-05 16:39:31 +0000

    :construction: home page with listing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 
