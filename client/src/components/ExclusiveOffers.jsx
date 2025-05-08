08e6f9935b85488000570675146690e784a851c4
Roopesh Kp <68722725+roopeshkp34@users.noreply.github.com>
2025-05-08 05:55:08 +0000

    :construction: Exclusive offers , testimonial and news letter

--- client/src/components/ExclusiveOffers.jsx
+++ client/src/components/ExclusiveOffers.jsx
@@ -9,10 +9,18 @@ const ExclusiveOffers = () => {
                 <Title align="left" title="Exclusive Offers" subTitle="Take advantage of our limited-time offers and special packages to enhance your stay and create unforgettable memmories."></Title>
                 <button className="group flex itemx-center gap-2 font-medium cursor-pointer max-md:mt-12">View All Offers <img src={assets.arrowIcon} alt="arrow-icon" className="group-hover:translate-x-1 transition-all" /></button>
             </div>
-            <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
                 {exclusiveOffers.map((item) => (
                     <div key={item._id} className="group relative flex flex-col items-start justify-between gap-1 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center" style={{backgroundImage: `url(${item.image})`}}>
                         <p className="px-3 py-1 absolute top-4 left-4 text-xs bg-white text-gray-800 font-medium rounded-full">{item.priceOff}% OFF</p>
+                        <div>
+                            <p className="text-2x1 font-medium font-playfair">{item.title}</p>
+                            <p className="">{item.description}</p>
+                            <p className="text-xs text-white/70 mt-3">Expires {item.expiryDate}</p>
+                        </div>
+                        <button className="flex items-center gap-2 font-medium cusrsor-pointer mt-4 mb-5">
+                            View Offers <img  className="invert group-hover:translate-x-1 transition-all" src={assets.arrowIcon} alt="arrow-icon" />
+                        </button>
 
                     </div>
                 ))}
